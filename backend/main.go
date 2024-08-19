0274b1706b6900bac18ebbfcd04c657d26038065
Fabien <fabien@clement.pt>
2024-08-19 16:01:26 +0200

    go air to update code real time in DEV

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,13 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "pong2",
+		})
+	})
+	//get all users
+	r.GET("/users", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "get all users",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
